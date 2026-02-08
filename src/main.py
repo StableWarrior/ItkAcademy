29c52f7379ce73e8e36217f1ccd248c494bb081c
Глеб Кибяков <gleserki@gmail.com>
2026-02-08 21:59:01 +1000

    Добавил конфигурацию fastapi

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from .database.connection import Base, engine
+from .routers import events_aggregator, events_provider, system
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(events_provider.router)
+app.include_router(events_aggregator.router)
+app.include_router(system.router)
+
+
+@app.exception_handler(Exception)
+async def http_exception_handler(_: Request, exc: Exception) -> JSONResponse:
+    error = {
+        "result": False,
+        "error_type": exc.__class__.__name__,
+        "error_message": exc.__str__(),
+    }
+    return JSONResponse(content=error)
