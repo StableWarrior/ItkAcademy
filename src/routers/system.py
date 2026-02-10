a2c66e02ee3d33fc31f2cc40ca02a6d3fd1cd1f6
Глеб Кибяков <gleserki@gmail.com>
2026-02-10 23:56:19 +1000

    Обновил system: Добавил /health /trigger

--- src/routers/system.py
+++ src/routers/system.py
@@ -1,6 +1,36 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from ..services.sync_metadata_service import SyncMetadataService
+from ..services.events_provider import EventsProviderService
+from ..services.events_paginator import EventsPaginatorService
+from ..database.repository.sync_metadata_repository import SyncMetadataRepository
+from ..shemas import SyncMetadata
 
 router = APIRouter(
     prefix="/api/v1",
     tags=["system"],
 )
+
+@router.get(
+    "/health",
+    name="health_check"
+)
+async def health_check():
+    return JSONResponse(content={"status": "ok"}, status_code=200)
+
+
+@router.post(
+    '/sync/trigger',
+    name='sync_metadata',
+    response_model=SyncMetadata
+)
+async def sync_metadata() -> SyncMetadata:
+
+    service = SyncMetadataService(
+        service=EventsPaginatorService(
+            service=EventsProviderService(),
+        ),
+        repository=SyncMetadataRepository()
+    )
+    metadata = await service.sync_events()
+    return metadata
