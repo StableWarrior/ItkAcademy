e07364a6c82c60193cd4b86cc8c084cd7db9775b
Глеб Кибяков <gleserki@gmail.com>
2026-02-10 23:53:25 +1000

    Добавил функции для получения событий с API

--- src/services/events_provider.py
+++ src/services/events_provider.py
@@ -1,2 +1,28 @@
+import aiohttp
+from ..config import X_API_KEY, EVENTS_API_URL
+
+
 class EventsProviderService:
-    pass
+
+
+    async def __aenter__(self):
+        self.session = aiohttp.ClientSession(
+            headers={
+                "x-api-key": X_API_KEY,
+            }
+        )
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if self.session:
+            await self.session.close()
+
+    async def get_first_page(self, changed_at: str):
+        async with self.session.get(f"{EVENTS_API_URL}/api/events/?changed_at={changed_at}") as response:
+            page = await response.json()
+        return page
+
+    async def get_page(self, url: str):
+        async with self.session.get(url) as response:
+            page = await response.json()
+        return page
