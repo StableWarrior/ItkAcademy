65b6d262b37e70bbf956bd1a31ba3321af50a38c
Глеб Кибяков <gleserki@gmail.com>
2026-02-11 19:24:04 +1000

    Добавил маршрут /api/events/{event_id}

--- src/routers/events_aggregator.py
+++ src/routers/events_aggregator.py
@@ -1,9 +1,12 @@
-from typing import Optional
 from datetime import date
+from typing import Optional
+from uuid import UUID
+
 from fastapi import APIRouter, Query
-from ..services.events_aggregator import EventsAggregatorService
+
 from ..database.repository.events_aggregator import EventsAggregatorRepository
-from ..shemas import Page
+from ..services.events_aggregator import EventsAggregatorService
+from ..shemas import Event, Page
 
 router = APIRouter(
     prefix="/api",
@@ -13,17 +16,24 @@ router = APIRouter(
 
 @router.get("/events", name="get_events", response_model=Page)
 async def get_events(
-        date_from: Optional[date] = Query(date(2000, 1, 1), description="Формат: YYYY-MM-DD"),
-        page: Optional[int] = Query(1, ge=1),
-        page_size: Optional[int] = Query(10, ge=1)
+    date_from: Optional[date] = Query(
+        date(2000, 1, 1), description="Формат: YYYY-MM-DD"
+    ),
+    page: Optional[int] = Query(1, ge=1),
+    page_size: Optional[int] = Query(10, ge=1),
 ) -> Page:
 
     service = EventsAggregatorService(
         repository=EventsAggregatorRepository(),
     )
-    page = await service.get_page(
-        date_from=date_from,
-        page=page,
-        page_size=page_size
-    )
+    page = await service.get_page(date_from=date_from, page=page, page_size=page_size)
     return page
+
+
+@router.get("/events/{event_id}", name="get_event", response_model=Event)
+async def get_event(event_id: UUID) -> Event:
+    service = EventsAggregatorService(
+        repository=EventsAggregatorRepository(),
+    )
+    event = await service.get_event(event_id=event_id)
+    return event
