548e61d629748cff5f4e5d5ee3e3ba481a498ba9
Глеб Кибяков <gleserki@gmail.com>
2026-02-11 17:11:38 +1000

    Реализовал сервис для выгрузки событий

--- src/services/events_aggregator.py
+++ src/services/events_aggregator.py
@@ -1,2 +1,20 @@
+from datetime import date
+from ..database.repository.events_aggregator import EventsAggregatorRepository
+
+
 class EventsAggregatorService:
-    pass
+
+    def __init__(self, repository: EventsAggregatorRepository):
+        self.repository = repository
+
+    async def get_page(
+        self,
+        date_from: date = date(2000, 1, 1),
+        page: int = 1,
+        page_size: int = 10,
+    ):
+        page = await self.repository.get_events(
+            date_from=date_from, page=page, page_size=page_size
+        )
+        return page
+
