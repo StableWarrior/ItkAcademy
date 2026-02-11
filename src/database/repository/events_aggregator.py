ebadfde43e2988da734d8f13536cd836b4e73da9
Глеб Кибяков <gleserki@gmail.com>
2026-02-11 17:10:44 +1000

    Добавил функции для выгрузки событий

--- src/database/repository/events_aggregator.py
+++ src/database/repository/events_aggregator.py
@@ -1,2 +1,56 @@
+from datetime import date
+from sqlalchemy import select, func
+from sqlalchemy.orm import joinedload
+
+from ..connection import async_session
+from ..models import Event
+from src.config import HOSTNAME, PORT
+
+
 class EventsAggregatorRepository:
-    pass
+
+    @classmethod
+    async def get_events(
+        cls,
+        date_from: date = date(2000, 1, 1),
+        page: int = 1,
+        page_size: int = 10,
+    ):
+        async with async_session() as db:
+            offset = (page - 1) * page_size
+
+            query = (
+                select(Event)
+                .where(Event.event_time >= date_from)
+                .options(joinedload(Event.place))
+                .order_by(Event.event_time)
+            )
+            count_query = (
+                select(func.count())
+                .select_from(Event)
+                .where(Event.event_time >= date_from)
+            )
+
+            result = await db.execute(
+                query.limit(page_size).offset(offset)
+            )
+
+            events = result.unique().scalars().all()
+            total_count = (await db.execute(count_query)).scalar_one()
+
+            next_url = None
+            if offset + page_size < total_count:
+                next_url = f"http://{HOSTNAME}:{PORT}/api/events/?page={page + 1}"
+
+            previous_url = None
+            if page > 1:
+                previous_url = f"http://{HOSTNAME}:{PORT}/api/events/?page={page - 1}"
+
+            response = {
+                "count": total_count,
+                "next": next_url,
+                "previous": previous_url,
+                "results": events,
+            }
+
+        return response
