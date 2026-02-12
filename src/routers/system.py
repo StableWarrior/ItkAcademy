ad67a9e8e171e4833ebaf493389158c632143977
Глеб Кибяков <gleserki@gmail.com>
2026-02-12 18:48:00 +1000

    Обновил system

--- src/routers/system.py
+++ src/routers/system.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
 from ..database.repository.sync_metadata_repository import SyncMetadataRepository
+from ..database.repository.events_aggregator import EventsAggregatorRepository
 from ..services.events_paginator import EventsPaginatorService
 from ..services.events_provider import EventsProviderService
 from ..services.sync_metadata_service import SyncMetadataService
+from ..services.events_aggregator import EventsAggregatorService
 from ..shemas import SyncMetadata
 
 router = APIRouter(
@@ -23,7 +25,11 @@ async def sync_metadata() -> SyncMetadata:
 
     service = SyncMetadataService(
         service=EventsPaginatorService(
-            service=EventsProviderService(),
+            service=EventsProviderService(
+                service=EventsAggregatorService(
+                    repository=EventsAggregatorRepository()
+                )
+            ),
         ),
         repository=SyncMetadataRepository(),
     )
