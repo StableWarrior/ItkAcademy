809c99674c372b8fb89806e9acd096fa6330d3c3
Глеб Кибяков <gleserki@gmail.com>
2026-02-15 17:40:23 +1000

    Добавил в сервис функции register_ticket, cansel_ticket

--- src/services/events_provider.py
+++ src/services/events_provider.py
@@ -1,9 +1,12 @@
 from uuid import UUID
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 import aiohttp
 from fastapi import HTTPException
 
-from ..config import EVENTS_API_URL, X_API_KEY
+from ..config import EVENTS_API_URL, X_API_KEY, LOGGER
+from ..shemas import Registration
 from .events_aggregator import EventsAggregatorService
 
 
@@ -41,9 +44,50 @@ class EventsProviderService:
             raise HTTPException(status_code=400, detail="Event is not published")
 
         async with self.session.get(
-            f"{EVENTS_API_URL}/api/events/{event_id}/seats"
+            f"{EVENTS_API_URL}/api/events/{event_id}/seats/"
         ) as response:
             seats = await response.json()
             seats["event_id"] = event_id
 
         return seats
+
+
+    async def register_ticket(self, registration: Registration):
+        seats = await self.get_seats(event_id=registration.event_id)
+        LOGGER.info("test", seats=seats["seats"])
+        event = await self.service.get_event(event_id=registration.event_id)
+
+        if not registration.seat in seats["seats"]:
+            raise HTTPException(status_code=404, detail="Seat is not found")
+        if event.registration_deadline <= datetime.now(ZoneInfo("Asia/Vladivostok")):
+            raise HTTPException(status_code=400, detail="Registration deadline has passed")
+
+        registration_data = registration.model_dump()
+        registration_data.pop("event_id")
+
+        async with self.session.post(
+            f"{EVENTS_API_URL}/api/events/{registration.event_id}/register/",
+            json=registration_data
+        ) as response:
+            ticket = await response.json()
+
+        await self.service.register_ticket(ticket_id=ticket["ticket_id"], registration=registration)
+
+        return ticket
+
+
+    async def cancel_ticket(self, ticket_id: UUID):
+        ticket = await self.service.get_ticket(ticket_id=ticket_id)
+
+        if ticket.event.event_time < datetime.now(ZoneInfo("Asia/Vladivostok")):
+            raise HTTPException(status_code=400, detail="Event already started")
+
+        async with self.session.delete(
+                f"{EVENTS_API_URL}/api/events/{ticket.event_id}/unregister/",
+                json={"ticket_id": str(ticket_id)}
+        ) as response:
+            status = await response.json()
+
+        await self.service.cansel_ticket(ticket_id=ticket_id)
+
+        return status
